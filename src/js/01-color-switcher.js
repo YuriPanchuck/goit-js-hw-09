71c08192fc0db64fbf7d15ef3a443ae262958d2b
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-10-29 23:38:02 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
 
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStopRef.addEventListener('click', onBtnStopClick);
+
+let idInterval = null;
+
+function onBtnStartClick() {
+  btnStartRef.disabled = true;
+  idInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick() {
+  clearInterval(idInterval);
+  btnStartRef.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
